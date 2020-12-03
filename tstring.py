dcbf37ef329e3da152ca6eaca3092db31142c18a
stanley-king <stanley.jiang@lrlz.com>
2020-12-03 21:18:25 +0800

    init

---
+++ tstring.py
@@ -0,0 +1,26 @@
+import unittest
+
+
+class StringCase(unittest.TestCase):
+    def test_pring(self):
+        print('1 C:\some\name')
+        print("2 C:\some\name")
+        print(r"3 C:\some\name")
+
+        print("""\
+                Usage: thingy [OPTIONS]
+                     -h                        Display this usage message
+                     -H hostname               Hostname to connect to
+                """)
+
+    def test_index(self):
+        word = 'Python'
+        #切片中的越界索引会被自动处理:
+        print(word[:3])
+        print(word[3:])
+        #Python 中的字符串不能被修改，它们是 immutable 的。
+        # 因此，向字符串的某个索引位置赋值会产生一个错误:
+
+
+if __name__ == '__main__':
+    unittest.main()
