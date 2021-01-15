ab31579be8d70cd48d45be8e639229fb523b36bc
stanley-king <stanley.jiang@lrlz.com>
2021-01-15 12:03:54 +0800

    debug cupy

--- tstring.py
+++ tstring.py
@@ -1,4 +1,10 @@
 import unittest
+import cupy as np
+np.cuda.set_allocator(np.cuda.MemoryPool().malloc)
+
+print('\033[92m' + '-' * 60 + '\033[0m')
+print(' ' * 23 + '\033[92mGPU Mode (cupy)\033[0m')
+print('\033[92m' + '-' * 60 + '\033[0m\n')
 
 
 class StringCase(unittest.TestCase):
